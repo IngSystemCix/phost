d495069b48c262fd525a4f27c899dab9c6aaf34d
IngSystemCix <juanromerocollazos@gmail.com>
2025-12-24 10:49:02 -0500

    feat: banner correction.

--- phost.ps1
+++ phost.ps1
@@ -199,7 +199,7 @@ function Invoke-Doctor {
     Get-NginxStatus
     Get-ApacheStatus
     Get-MariaDBStatus
-    Write-Host "\`nDiagnostics complete" -ForegroundColor Green
+    Write-Host "`nDiagnostics complete" -ForegroundColor Green
     Pause
 }
 
